e8e03f06f0d1e002baf00d6f6ad472309e2c135e
CoolShivas <shivas2710cool00@gmail.com>
2025-03-15 12:11:58 +0530

    Introduction to NPM

---
+++ behindNodeJS/theory.js
@@ -0,0 +1,27 @@
+/***
+ * 
+ * 
+ * * * * * Introduction to npm :-
+ * 
+ * 
+        1.) npm is a popular package manager which comes bundled with Node.js.
+        
+        2.) It is a CLI tool used to install, update, and remove external packages.
+
+        3.)You can also create your own package and publish it on npmjs.com registry.
+
+        4.) Do not confuse npm CLI with npmjs.com, as it’s a registry where most of the 
+            packages of Node.js are saved.
+            • There are alternative Node.js package managers registry like JSR, which we will discuss in future.
+        5.) Fun facts:-
+                • npm shouldn’t be written in capitalized form unless you have everything as capital.
+                • npm doesn’t stand for Node Package Manager even though many people refer to it as 
+                  that. It is a recursive bacronymic abbreviation for "npm is not an acronym“.
+
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
