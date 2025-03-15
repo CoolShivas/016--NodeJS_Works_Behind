8fdec524a39c5e8cb82797167e19c7f25ab5b8eb
CoolShivas <shivas2710cool00@gmail.com>
2025-03-15 14:49:41 +0530

    Node.js Works behind the Scene with Sync and Async Task practical .

--- behindNodeJS/newFile.js
+++ behindNodeJS/newFile.js
@@ -13,3 +13,15 @@ console.log("Sync :-", readData);
 // // // // Sync :- My name is Lakhan.
 // // // // Synchronous Task also known as the Blocking Task.
 // // // // Therefore, console.log("Start"); have to run first then only the next will run i.e., (console.log("Sync :-", readData);)
+
+
+
+fs.readFile('sync.txt', 'utf-8', (err, data) => {
+    if(err) throw err;
+    console.log("Async :-", data);
+});
+// // // // Here, we are getting the Output as Terminal as :-
+// // // // Start
+// // // // Sync :- My name is Lakhan.
+// // // // Async :- My name is Lakhan.
+// // // // Therefore, Synchronous Task execute first then only after its completion Asynchronous will execute. Whereas, Asynchronous Task do not put the rest of the code to wait. If it is taking time it will not block the path of rest of the code such as Synchronous do.
